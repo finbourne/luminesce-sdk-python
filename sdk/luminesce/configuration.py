0ba6f73a1ff7eae61efdf1818ff2c5650a02f318
Concourse <concourse@finbourne.com>
2023-11-17 22:19:58 +0000

    Updates for v2.0.1

--- sdk/luminesce/configuration.py
+++ sdk/luminesce/configuration.py
@@ -373,7 +373,7 @@ class Configuration:
         return "Python SDK Debug Report:\n"\
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
-               "Version of the API: 1.13.930\n"\
+               "Version of the API: 1.13.934\n"\
                "SDK Package Version: {package_version}".\
                format(env=sys.platform, pyversion=sys.version, package_version=package_version)
 
