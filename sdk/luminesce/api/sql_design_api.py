3be8a5c0424e0c77daa95d2b2be00fdebd035073
Concourse <concourse@finbourne.com>
2024-09-30 19:21:56 +0000

    luminesce python updates for v2.1.146
    Signed-off-by: Concourse <concourse@finbourne.com>

--- sdk/luminesce/api/sql_design_api.py
+++ sdk/luminesce/api/sql_design_api.py
@@ -22,7 +22,7 @@ from typing import overload, Optional, Union, Awaitable
 from typing_extensions import Annotated
 from pydantic.v1 import Field, StrictBool, StrictInt, StrictStr
 
-from typing import List, Optional
+from typing import List, Optional, Union
 
 from luminesce.models.case_statement_design import CaseStatementDesign
 from luminesce.models.convert_to_view_data import ConvertToViewData
@@ -57,6 +57,166 @@ class SqlDesignApi:
         if api_client is None:
             api_client = ApiClient.get_default()
         self.api_client = api_client
+
+    @overload
+    async def get_provider_template_for_export(self, provider : Annotated[StrictStr, Field(..., description="Name of the provider for which this template is for")], content_type : Annotated[StrictStr, Field(..., description="File content type for the Template. csv or excel")], **kwargs) -> bytearray:  # noqa: E501
+        ...
+
+    @overload
+    def get_provider_template_for_export(self, provider : Annotated[StrictStr, Field(..., description="Name of the provider for which this template is for")], content_type : Annotated[StrictStr, Field(..., description="File content type for the Template. csv or excel")], async_req: Optional[bool]=True, **kwargs) -> bytearray:  # noqa: E501
+        ...
+
+    @validate_arguments
+    def get_provider_template_for_export(self, provider : Annotated[StrictStr, Field(..., description="Name of the provider for which this template is for")], content_type : Annotated[StrictStr, Field(..., description="File content type for the Template. csv or excel")], async_req: Optional[bool]=None, **kwargs) -> Union[bytearray, Awaitable[bytearray]]:  # noqa: E501
+        """GetProviderTemplateForExport: Makes a fields template for file importing via a writer  # noqa: E501
+
+        Generates a template file for all the writable fields for a given provider returned in CSV or Excel (xlsx) format (as a file to be downloaded)  # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+
+        >>> thread = api.get_provider_template_for_export(provider, content_type, async_req=True)
+        >>> result = thread.get()
+
+        :param provider: Name of the provider for which this template is for (required)
+        :type provider: str
+        :param content_type: File content type for the Template. csv or excel (required)
+        :type content_type: str
+        :param async_req: Whether to execute the request asynchronously.
+        :type async_req: bool, optional
+        :param _request_timeout: Timeout setting. Do not use - use the opts parameter instead
+        :param opts: Configuration options for this request
+        :type opts: ConfigurationOptions, optional
+        :return: Returns the result object.
+                 If the method is called asynchronously,
+                 returns the request thread.
+        :rtype: bytearray
+        """
+        kwargs['_return_http_data_only'] = True
+        if '_preload_content' in kwargs:
+            message = "Error! Please call the get_provider_template_for_export_with_http_info method with `_preload_content` instead and obtain raw data from ApiResponse.raw_data"  # noqa: E501
+            raise ValueError(message)
+        if async_req is not None:
+            kwargs['async_req'] = async_req
+        return self.get_provider_template_for_export_with_http_info(provider, content_type, **kwargs)  # noqa: E501
+
+    @validate_arguments
+    def get_provider_template_for_export_with_http_info(self, provider : Annotated[StrictStr, Field(..., description="Name of the provider for which this template is for")], content_type : Annotated[StrictStr, Field(..., description="File content type for the Template. csv or excel")], **kwargs) -> ApiResponse:  # noqa: E501
+        """GetProviderTemplateForExport: Makes a fields template for file importing via a writer  # noqa: E501
+
+        Generates a template file for all the writable fields for a given provider returned in CSV or Excel (xlsx) format (as a file to be downloaded)  # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+
+        >>> thread = api.get_provider_template_for_export_with_http_info(provider, content_type, async_req=True)
+        >>> result = thread.get()
+
+        :param provider: Name of the provider for which this template is for (required)
+        :type provider: str
+        :param content_type: File content type for the Template. csv or excel (required)
+        :type content_type: str
+        :param async_req: Whether to execute the request asynchronously.
+        :type async_req: bool, optional
+        :param _preload_content: if False, the ApiResponse.data will
+                                 be set to none and raw_data will store the
+                                 HTTP response body without reading/decoding.
+                                 Default is True.
+        :type _preload_content: bool, optional
+        :param _return_http_data_only: response data instead of ApiResponse
+                                       object with status code, headers, etc
+        :type _return_http_data_only: bool, optional
+        :param _request_timeout: Timeout setting. Do not use - use the opts parameter instead
+        :param opts: Configuration options for this request
+        :type opts: ConfigurationOptions, optional
+        :param _request_auth: set to override the auth_settings for an a single
+                              request; this effectively ignores the authentication
+                              in the spec for a single request.
+        :type _request_auth: dict, optional
+        :type _content_type: string, optional: force content-type for the request
+        :return: Returns the result object.
+                 If the method is called asynchronously,
+                 returns the request thread.
+        :rtype: tuple(bytearray, status_code(int), headers(HTTPHeaderDict))
+        """
+
+        _params = locals()
+
+        _all_params = [
+            'provider',
+            'content_type'
+        ]
+        _all_params.extend(
+            [
+                'async_req',
+                '_return_http_data_only',
+                '_preload_content',
+                '_request_timeout',
+                '_request_auth',
+                '_content_type',
+                '_headers',
+                'opts'
+            ]
+        )
+
+        # validate the arguments
+        for _key, _val in _params['kwargs'].items():
+            if _key not in _all_params:
+                raise ApiTypeError(
+                    "Got an unexpected keyword argument '%s'"
+                    " to method get_provider_template_for_export" % _key
+                )
+            _params[_key] = _val
+        del _params['kwargs']
+
+        _collection_formats = {}
+
+        # process the path parameters
+        _path_params = {}
+
+        # process the query parameters
+        _query_params = []
+        if _params.get('provider') is not None:  # noqa: E501
+            _query_params.append(('provider', _params['provider']))
+
+        if _params.get('content_type') is not None:  # noqa: E501
+            _query_params.append(('contentType', _params['content_type']))
+
+        # process the header parameters
+        _header_params = dict(_params.get('_headers', {}))
+        # process the form parameters
+        _form_params = []
+        _files = {}
+        # process the body parameter
+        _body_params = None
+        # set the HTTP header `Accept`
+        _header_params['Accept'] = self.api_client.select_header_accept(
+            ['text/plain', 'application/json', 'text/json'])  # noqa: E501
+
+        # authentication setting
+        _auth_settings = ['oauth2']  # noqa: E501
+
+        _response_types_map = {
+            '200': "bytearray",
+            '400': "LusidProblemDetails",
+            '403': "LusidProblemDetails",
+        }
+
+        return self.api_client.call_api(
+            '/api/Sql/providertemplateforexport', 'GET',
+            _path_params,
+            _query_params,
+            _header_params,
+            body=_body_params,
+            post_params=_form_params,
+            files=_files,
+            response_types_map=_response_types_map,
+            auth_settings=_auth_settings,
+            async_req=_params.get('async_req'),
+            _return_http_data_only=_params.get('_return_http_data_only'),  # noqa: E501
+            _preload_content=_params.get('_preload_content', True),
+            _request_timeout=_params.get('_request_timeout'),
+            opts=_params.get('opts'),
+            collection_formats=_collection_formats,
+            _request_auth=_params.get('_request_auth'))
 
     @overload
     async def put_case_statement_design_sql_to_design(self, body : Annotated[Optional[StrictStr], Field(description="SQL to attempt to create an case statement Design object from")] = None, **kwargs) -> CaseStatementDesign:  # noqa: E501
