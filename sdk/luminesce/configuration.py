79918bfab936d04ba214d490a3b7e7a0a9f66a68
Concourse <concourse@finbourne.com>
2025-03-20 07:43:03 +0000

    luminesce python updates for v2.1.236
    Signed-off-by: Concourse <concourse@finbourne.com>

--- sdk/luminesce/configuration.py
+++ sdk/luminesce/configuration.py
@@ -445,7 +445,7 @@ class Configuration:
         return "Python SDK Debug Report:\n"\
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
-               "Version of the API: 1.18.163\n"\
+               "Version of the API: 1.18.177\n"\
                "SDK Package Version: {package_version}".\
                format(env=sys.platform, pyversion=sys.version, package_version=package_version)
 
